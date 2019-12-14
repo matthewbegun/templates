f7005cecd8c8a9e7d60e672e1f500248da16ada9
Matthew Begun <matthew.begun@gmail.com>
2019-12-14 18:26:24 +1100

    first

---
+++ install.R
@@ -0,0 +1,11 @@
+### install regular packages
+
+install.packages("reticulate") # python support in RMarkdown
+install.packages("ggplot2") # for plotting
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
